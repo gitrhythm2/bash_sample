59b9ec8cdeed8c4d8596af99bf8e8823584e71e8
Hiroyuki Hosokawa <simamaru@gmail.com>
2021-04-17 20:25:19 +0900

    Add variables/shell_variables.sh

--- main.sh
+++ main.sh
@@ -2,3 +2,5 @@
 
 # bash manページ: https://linuxjm.osdn.jp/html/GNU_bash/man1/bash.1.html
 
+# - シェル変数
+./variables/shell_variables.sh
---
+++ variables/shell_variables.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# シェル変数(Shell Variables)
+
+# HOME
+echo "HOME: ユーザーのホームディレクトリ[${HOME}]"
+# PWD
+echo "PWD: カレントディレクトリ[${PWD}]"
+cd $HOME
+echo "     cd $HOME'するとカレントディレクトリも変わる[${PWD}]"
+# SHELL
+echo "SHELL: ログインシェルのフルパス[${SHELL}]"
+# BASH
+echo "BASH: 現在動作しているbashコマンドのフルパス[${BASH}]"
+# BASH_VERSION
+echo "BASH_VERSION: 現在動作しているbashのバージョン'[${BASH_VERSION}]"
+# BASH_VERSINFO
+echo "BASH_VERSINFO: 読み込み専用の配列(各要素の詳細はmanを参照)[${BASH_VERSINFO[@]}]"
+# LINENO
+echo "LINENO: 現在実行しているスクリプトの行番号(主にデバッグ用途)[${LINENO}]"
+# LANG
+echo "LANG: ロケール(地域や言語の指定)[${LANG}]"
+# IFS
+echo 'IFS: シェルの区切り文字 >ecoh "${IFS}" | od -a'
+echo "${IFS}" | od -a
